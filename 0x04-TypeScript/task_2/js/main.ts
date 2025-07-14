aca1a2cc98e857f3197859b80bde8388f63c6744
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-14 16:59:11 +0200

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -62,10 +62,25 @@ function executeWork(employee: Director | Teacher): string {
   }
 }
 
-// Example output
-console.log(createEmployee(200));   // Teacher instance
-console.log(createEmployee(1000));  // Director instance
-console.log(createEmployee('$500')); // Director instance
+// String literal type
+type Subjects = 'Math' | 'History';
 
-console.log(executeWork(createEmployee(200)));   // Getting to work
-console.log(executeWork(createEmployee(1000)));  // Getting to director tasks
+// Function using Subjects type
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
+
+// Example output
+console.log(createEmployee(200));      // Teacher instance
+console.log(createEmployee(1000));     // Director instance
+console.log(createEmployee('$500'));   // Director instance
+
+console.log(executeWork(createEmployee(200)));    // Getting to work
+console.log(executeWork(createEmployee(1000)));   // Getting to director tasks
+
+console.log(teachClass('Math'));       // Teaching Math
+console.log(teachClass('History'));    // Teaching History
